d8510af2fbb854a29522d77b1a2217e34e42b8f4
AlanDXIV <127701652+AlanDXIV@users.noreply.github.com>
2023-03-13 08:22:30 +0800

    initial commit

---
+++ TestSettings.lua
@@ -0,0 +1,31 @@
+local tbl = 
+{
+	fakeSettingCheckbox1 = {
+		type = "checkbox",
+		label = "fake setting name",
+		tooltip = "fake hover tooltip",
+		bool = false,
+	},
+	fakeSettingCheckbox2 = {
+		type = "checkbox",
+		label = "fake setting name 2",
+		tooltip = "fake hover tooltip 2",
+		bool = true,
+	},
+	fakeSettingNumber1 = {
+		type = "number",
+		label = "fake number setting name",
+		tooltip = "fake number hover tooltip",
+		num = 50,
+	},
+	fakeSettingNumber2 = {
+		type = "number",
+		label = "fake number setting name 2",
+		tooltip = "fake number hover tooltip 2 ",
+		num = 100,
+	},
+}
+
+
+
+return tbl
